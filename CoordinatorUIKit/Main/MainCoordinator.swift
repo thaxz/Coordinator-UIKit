a0b44794f4e78f0f27a51b3213616b335b83b8b3
thaxz <thaxzt@gmail.com>
2023-08-27 19:33:43 -0300

    feature(Develop): Configuring first and second tabs

--- CoordinatorUIKit/Main/MainCoordinator.swift
+++ CoordinatorUIKit/Main/MainCoordinator.swift
@@ -18,12 +18,23 @@ class MainCoordinator: Coordinator {
         let firstCoordinator = FirstTabCoordinator()
         firstCoordinator.start()
         self.childCoordinator.append(firstCoordinator)
+        let firstViewController = firstCoordinator.rootViewController
+        setup(vc: firstViewController, title: "First Tab", imageName: "paperplane", selectedImageName: "paperplane.fill")
+        
         let secondCoordinator = SecondTabCoordinator()
         secondCoordinator.start()
         self.childCoordinator.append(secondCoordinator)
+        let secondViewController = secondCoordinator.rootViewController
+        setup(vc: firstViewController, title: "Second Tab", imageName: "bell", selectedImageName: "bell.fill")
         
-        self.rootViewController.viewControllers = [firstCoordinator.rootViewController, secondCoordinator.rootViewController] 
+        self.rootViewController.viewControllers = [firstViewController, secondViewController]
     }
     
+    func setup(vc: UIViewController, title: String, imageName: String, selectedImageName: String){
+        let defaultImage = UIImage(systemName: imageName)
+        let selectedImage = UIImage(systemName: selectedImageName)
+        let tabBarItem = UITabBarItem(title: title, image: defaultImage, selectedImage: selectedImage)
+        vc.tabBarItem = tabBarItem
+    }
     
 }
