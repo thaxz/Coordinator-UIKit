427a04f1f280d7fbdfdf3ad253a783bead3e4300
thaxz <thaxzt@gmail.com>
2023-08-27 19:15:44 -0300

    feature(Develop): Creating FirstView controller using uikit

---
+++ CoordinatorUIKit/Main/First/FirstDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstDetailView.swift
+//  CoordinatorUIKit
+//
+//  Created by thaxz on 27/08/23.
+//
+
+import SwiftUI
+
+struct FirstDetailView: View {
+    var body: some View {
+        Text("First Detail using SwiftUI")
+    }
+}
+
+struct FirstDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstDetailView()
+    }
+}
---
+++ CoordinatorUIKit/Main/First/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  CoordinatorUIKit
+//
+//  Created by thaxz on 27/08/23.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
